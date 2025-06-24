7425771f80861318e97f07686683a2cc9ce7ed4c
Latte081802 <123555976+Latte081802@users.noreply.github.com>
2025-06-25 02:09:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 
 from flask import Flask, request, jsonify
 import numpy as np
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.SystemClock
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.Interpreter
+import org.tensorflow.lite.gpu.CompatibilityList
+import org.tensorflow.lite.gpu.GpuDelegate
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.common.ops.CastOp
+import org.tensorflow.lite.support.common.ops.NormalizeOp
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
 from PIL import Image
 import io
 
